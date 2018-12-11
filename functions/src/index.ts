5fc9f902b871326866d900e93c3e1aa9622b1b8f
François GRUCHALA <francois.gruchala@gmail.com>
2018-12-11 15:30:41 +0100

    :sparkles: initialize app

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,7 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
 
 import findAllTweetsByQuery from './twitter/functions/find-all-tweets-by-query';
 import findAndShuffleAllParticipantsForTwitterDraw from './draws/functions/find-and-shuffle-all-participants-twitter-draw';
